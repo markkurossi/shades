73c5c4b113e33628f49c4dacc64f03b0d1eee3a6
Markku Rossi <mtr@iki.fi>
2024-11-13 08:17:17 +0100

    Implemented ReadAt and WriteAt.

--- db/memdevice.go
+++ db/memdevice.go
@@ -6,8 +6,20 @@
 
 package db
 
+import (
+	"fmt"
+)
+
 // MemDevice implements memory device.
 type MemDevice struct {
+	buf []byte
+}
+
+// NewMemDevice creates a new memory device with the size capacity.
+func NewMemDevice(size int) *MemDevice {
+	return &MemDevice{
+		buf: make([]byte, size),
+	}
 }
 
 // Close implements Device.Close.
@@ -17,7 +29,11 @@ func (mem *MemDevice) Close() error {
 
 // ReadAt implements Device.ReadAt.
 func (mem *MemDevice) ReadAt(b []byte, off int64) (n int, err error) {
-	return len(b), nil
+	if int(off)+len(b) > len(mem.buf) {
+		return 0, fmt.Errorf("reading %v bytes out of range [0...%v[",
+			int(off)+len(b)-len(mem.buf), len(mem.buf))
+	}
+	return copy(b, mem.buf[off:]), nil
 }
 
 // Sync implements Device.Sync.
@@ -27,5 +43,9 @@ func (mem *MemDevice) Sync() error {
 
 // WriteAt implements Device.WriteAt.
 func (mem *MemDevice) WriteAt(b []byte, off int64) (n int, err error) {
-	return len(b), nil
+	if int(off)+len(b) > len(mem.buf) {
+		return 0, fmt.Errorf("writing %v bytes out of range [0...%v[",
+			int(off)+len(b)-len(mem.buf), len(mem.buf))
+	}
+	return copy(mem.buf[off:], b), nil
 }
